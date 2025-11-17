5a560bdb209af6a0f105c18dbf1cb98dce0a4dc0
George Smith <georgepetersmith@outlook.com>
2025-11-17 23:45:59 +0000

    Organise use statements

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,8 +1,8 @@
-use budgie::Budget;
-use leptos::prelude::*;
 use crate::components::budget_summary::BudgetSummary;
 use crate::components::expenditure::Expenditure;
 use crate::components::income::Income;
+use budgie::Budget;
+use leptos::prelude::*;
 
 #[component]
 pub fn App() -> impl IntoView {
